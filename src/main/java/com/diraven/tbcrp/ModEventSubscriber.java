6df5a914ecdcf32bf50039795181407aa206b93a
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2020-06-01 23:44:33 +0300

    Add empty custom item.

---
+++ src/main/java/com/diraven/tbcrp/ModEventSubscriber.java
@@ -0,0 +1,28 @@
+package com.diraven.tbcrp;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+@EventBusSubscriber(modid = Main.MODID)
+public class ModEventSubscriber {
+    @SubscribeEvent
+    public static void onRegisterItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(
+                setup(new Item(), "example_item")
+        );
+
+    }
+
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
+        return setup(entry, new ResourceLocation(Main.MODID, name));
+    }
+
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final ResourceLocation registryName) {
+        entry.setRegistryName(registryName);
+        return entry;
+    }
+}
