694979e0e33da0e8d500fc257eaa749ead3fbf1c
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2020-06-04 22:11:51 +0300

    Obj model test.

--- src/main/java/com/diraven/tbcrp/ModBlocks.java
+++ src/main/java/com/diraven/tbcrp/ModBlocks.java
@@ -9,5 +9,5 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Main.MOD_ID);
 
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(Block.Properties.create(Material.ROCK)));
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f, 3.0f)));
 }
