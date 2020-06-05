86ab23cd313d8b03753d38a6155b174bbcd46e3b
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2020-06-05 22:03:58 +0300

    Add first custom block - washing machine.

--- src/main/java/com/diraven/tbcrp/ModBlocks.java
+++ src/main/java/com/diraven/tbcrp/ModBlocks.java
@@ -1,5 +1,6 @@
 package com.diraven.tbcrp;
 
+import com.diraven.tbcrp.block.WashingMachine;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.fml.RegistryObject;
@@ -9,5 +10,5 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Main.MOD_ID);
 
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f, 3.0f)));
+    public static final RegistryObject<Block> WASHING_MACHINE = BLOCKS.register("washing_machine", () -> new WashingMachine(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f, 3.0f)));
 }
